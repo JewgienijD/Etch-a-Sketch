65dbed821befef4642ef0dbd373115ba1fdb4932
SirJohnSem <jewgienijdanilczik@mail.ru>
2021-06-28 21:25:17 +0200

    add new functionality

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
 const gridContainer = document.getElementById('grid-container');
+const cleanGridButton = document.getElementById('clean');
+const rangeGridSize = document.getElementById('grid-numbers');
 
 function makeSquare(rows,colms){
+    remoweSquare();
     gridContainer.style.setProperty('--grid-rows', rows);
     gridContainer.style.setProperty('--grid-colms', colms);
     for (i=0; i<rows*colms; i++){
@@ -16,5 +19,30 @@ function makeSquare(rows,colms){
    })
 };
 
-makeSquare(24,24);
+//clean grid by clicking "clean" button
+function getCleanGrid(){
+let cleanPixels = gridContainer.querySelectorAll('div');
+cleanPixels.forEach(griditem=> griditem.style.backgroundColor = '#ffffff');
+}
+cleanGridButton.addEventListener('click', getCleanGrid); 
+
+//create newsize grid by changing 'range - input'
+rangeGridSize.addEventListener('change', function(){
+    getCleanGrid();
+    makeSquare(rangeGridSize.value, rangeGridSize.value);
+})
+
+//remowe old grid 
+function remoweSquare(){
+    while(gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+makeSquare(10,10);
+
+
+
+
+
 
